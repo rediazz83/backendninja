8561b8a08eccdbaf3a3d64157bf255b6d16b5af1
Rodrigo Diaz <esteban.diaz@globant.com>
2018-11-01 20:12:53 -0300

    Integrando capas Controller y Service

---
+++ src/main/java/com/udemy/backendninja/controller/CourseController.java
@@ -0,0 +1,39 @@
+package com.udemy.backendninja.controller;
+
+import com.udemy.backendninja.entity.Course;
+import com.udemy.backendninja.service.CourseService;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+@ControllerAdvice
+@RequestMapping("/courses")
+public class CourseController {
+
+    @Autowired
+    private CourseService courseService;
+
+    public static final String COURSES_VIEW = "courses";
+    private static final Log LOGGER = LogFactory.getLog(CourseController.class);
+
+    @GetMapping("/list-courses")
+    public ModelAndView listAllCourses() {
+        LOGGER.info("METHOD: listAllCourses");
+
+        ModelAndView response = new ModelAndView(COURSES_VIEW);
+        response.addObject("courses", courseService.listAllCourses());
+
+        LOGGER.info("TEMPLATE: " + COURSES_VIEW);
+        return response;
+    }
+
+    @PostMapping("/add-course")
+    public String addCourse(@ModelAttribute("course")Course course) {
+        LOGGER.info("METHOD: addCourse -- PARAMS: " + course);
+
+        courseService.addCourse(course);
+        return "redirect:/list-courses";
+    }
+}
