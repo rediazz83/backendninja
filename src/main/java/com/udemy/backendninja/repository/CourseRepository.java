165cd1a6d7cce672b590b153cd45b8ec24608726
Rodrigo Diaz <esteban.diaz@globant.com>
2018-11-01 18:57:52 -0300

    Primer repo JPA

---
+++ src/main/java/com/udemy/backendninja/repository/CourseRepository.java
@@ -0,0 +1,13 @@
+package com.udemy.backendninja.repository;
+
+import com.udemy.backendninja.entity.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.io.Serializable;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Serializable> {
+
+    abstract Course findById(int id);
+}
--- src/main/java/com/udemy/backendninja/service/impl/ExampleServiceImpl.java
+++ src/main/java/com/udemy/backendninja/service/impl/ExampleServiceImpl.java
@@ -1,9 +1,11 @@
 package com.udemy.backendninja.service.impl;
 
 import com.udemy.backendninja.model.Person;
+import com.udemy.backendninja.repository.CourseRepository;
 import com.udemy.backendninja.service.ExampleService;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -14,9 +16,14 @@ public class ExampleServiceImpl implements ExampleService {
 
     private static final Log LOGGER = LogFactory.getLog(ExampleServiceImpl.class);
 
+    @Autowired
+    private CourseRepository courseRepository;
+
     @Override
     public List<Person> getPeople() {
         LOGGER.info("METHOD: getPeople");
+
+        courseRepository.findById(1);
 
         List<Person> people = new ArrayList<Person>();
         people.add(new Person("Rodrigo", 35));
